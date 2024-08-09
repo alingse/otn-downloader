41e6d9f0a6a5d31829f24e6f37241c237ad4ae50
alingse <alingse@foxmail.com>
2024-08-09 18:09:31 +0800

    use html5qrcode

--- server/server.go
+++ server/server.go
@@ -1,5 +1,53 @@
 package server
 
-func Run() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func Run() {
+	log.Println("Starting our simple http server.")
+
+	// Registering our handler functions, and creating paths.
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/info", Info)
+
+	log.Println("Started on port", 8001)
+	fmt.Println("To close connection CTRL+C :-)")
+
+	// Spinning up the server.
+	err := http.ListenAndServe(":8001", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, indexHtml)
+}
+
+func Info(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Info page")
+}
+
+const indexHtml = `
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OTN Downloader Server</title>
+</head>
+<body>
+<script src="https://unpkg.com/html5-qrcode@2.0.9/dist/html5-qrcode.min.js"></script>
+<div style="width: 500px" id="reader"></div>
+<script>
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(decodedText, decodedResult);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
+</script>
+</body>
+</html>`
